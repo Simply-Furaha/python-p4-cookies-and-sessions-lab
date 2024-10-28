a267ac30ecf46537cf576118a5dfde3413b5f71d
Maxwell Furaha <maxwellfuraha91@gmail.com>
2024-10-28 15:28:21 +0300

    Solution

--- server/app.py
+++ server/app.py
@@ -3,7 +3,7 @@
 from flask import Flask, make_response, jsonify, session
 from flask_migrate import Migrate
 
-from models import db, Article, User
+from models import db, Article
 
 app = Flask(__name__)
 app.secret_key = b'Y\xf1Xz\x00\xad|eQ\x80t \xca\x1a\x10K'
@@ -22,13 +22,29 @@ def clear_session():
 
 @app.route('/articles')
 def index_articles():
-
-    pass
-
+    articles =[article.to_dict() for article in Article.query.all()]
+    return make_response(jsonify(articles), 200) 
+    
 @app.route('/articles/<int:id>')
 def show_article(id):
+    session['page_views'] = session.get('page_views', 0)
+    session['page_views'] += 1
+    print(f"Current session page views: {session['page_views']}")
+    # if session['page_views'] > 3:
+    #     return make_response({"message": "Maximum pageview limit reached"}, 401)
 
-    pass
+    # article = Article.query.filter_by(id=id).first()
+
+    # if article:
+    #     return make_response(jsonify(article.to_dict()), 200)
+    # else:
+    #     return make_response({"error": "Article not found"}, 404)
+    if session['page_views'] <= 3:
+        article = Article.query.filter_by(id=id).first().to_dict()
+        return make_response(jsonify(article), 200)
+    else:
+        return make_response({"message": "Maximum pageview limit reached"}, 401)
+    
 
 if __name__ == '__main__':
     app.run(port=5555)
